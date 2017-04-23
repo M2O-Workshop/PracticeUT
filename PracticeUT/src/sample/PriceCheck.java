34491529a62b0a4a2f7772f808c1ef31c27bad6d
mizumoto-r <mizumoto.ryosuke@eiwa.com>
2017-04-23 17:11:10 +0900

    2017/4/23 修正
    コンストラクタの汎用性を高めるように修正

--- PracticeUT/src/sample/PriceCheck.java
+++ PracticeUT/src/sample/PriceCheck.java
@@ -1,15 +1,16 @@
 /**
  * Copyright 2017 EIS Co., Ltd. All rights reserved.
  */
+
 package sample;
 
 import java.math.BigDecimal;
 
 /**
  * @author 水元 <br />
- *  金額決定クラス <br />
- *  金額、会員情報、クーポン情報を取得し、最終的な請求金額を決定する <br />
- *  更新履歴 2017/04/05 水元：新規作成 <br />
+ * 金額決定クラス <br />
+ * 金額、会員情報、クーポン情報を取得し、最終的な請求金額を決定する <br />
+ * 更新履歴 2017/04/05 水元：新規作成 <br />
  */
 public class PriceCheck {
   // 金額
@@ -20,52 +21,54 @@ public class PriceCheck {
   private String coupon;
   // 消費税込
   private String tax;
-  
+
   /**
    * 値設定コンストラクタ <br />
    * 処理に使用するオブジェクトに値を設定する <br />
+   * @param price 金額
+   * @param member 会員
+   * @param coupon クーポン
    */
-  PriceCheck() {
-    this.price = new BigDecimal("100000");
-    this.member = true;
-    this.coupon = "2";
+  PriceCheck( BigDecimal price, boolean member, String coupon ) {
+    this.price = price;
+    this.member = member;
+    this.coupon = coupon;
     this.tax = "1.08";
   }
-  
+
   /**
    * 金額決定クラス <br />
    * 金額、会員情報、クーポン情報を取得し、最終的な請求金額を決定する <br />
    * @throws Exception 例外
    * @return price 金額
    */
-  public static BigDecimal priceReturn()throws Exception {
+  public BigDecimal priceReturn() throws Exception {
     // 返り値格納用
-    BigDecimal finalPrice = new BigDecimal(0);
-    // コンストラクタの取得
-    PriceCheck priceCheck = new PriceCheck();
+    BigDecimal finalPrice = new BigDecimal( 0 );
+    
     // クーポンの種類によって割引
-    switch (priceCheck.coupon) {
-      case "0":
-        break;
-      case "1":
-        finalPrice = priceCheck.price.multiply(new BigDecimal("0.95"));
-        break;
-      case "2":
-        finalPrice = priceCheck.price.multiply(new BigDecimal("0.90"));
-        break;
-      case "3":
-        finalPrice = priceCheck.price.multiply(new BigDecimal("0.80"));
-        break;
-      default:
-        break;
+    if (this.coupon.equals( "0" )) {
+      finalPrice = this.price;
+      
+    } else if (this.coupon.equals( "1" )) {
+      finalPrice = this.price.multiply( new BigDecimal( "0.95" ) );
+      
+    } else if (this.coupon.equals( "2" )) {
+      finalPrice = this.price.multiply( new BigDecimal( "0.90" ) );
+      
+    } else if (this.coupon.equals( "3" )) {
+      finalPrice = this.price.multiply( new BigDecimal( "0.80" ) );
+      
+    } else {
+      finalPrice = this.price;
     }
     
     // 会員の場合、消費税カット
-    if (!priceCheck.member) {
-      finalPrice = finalPrice.multiply( new BigDecimal(priceCheck.tax) );
+    if ( !this.member ) {
+      finalPrice = finalPrice.multiply( new BigDecimal( this.tax ) );
     }
     
     // 最終的な金額を返す（小数点以下は繰り上げ）
-    return finalPrice.setScale(0, BigDecimal.ROUND_UP);
+    return finalPrice.setScale( 0, BigDecimal.ROUND_UP );
   }
 }
