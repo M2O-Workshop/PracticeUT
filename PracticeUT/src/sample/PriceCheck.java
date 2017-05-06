76a345ed2b9e7dbd7a886a4eeadf7200ee129ef0
mizumoto-r <mizumoto.ryosuke@eiwa.com>
2017-05-06 18:56:14 +0900

    2017/5/6修正
    コンストラクタを削除し、priceCalメソッドに引数を追加

--- PracticeUT/src/sample/PriceCheck.java
+++ PracticeUT/src/sample/PriceCheck.java
@@ -8,65 +8,51 @@ import java.math.BigDecimal;
 
 /**
  * @author 水元 <br />
- * 支払金額計算クラス <br />
- * 定価、会員、割引情報を取得し、最終的な支払金額を計算する <br />
- * 更新履歴 2017/04/05 水元：新規作成 <br />
+ *         支払金額計算クラス <br />
+ *         定価、会員、割引情報を取得し、最終的な支払金額を計算する <br />
+ *         更新履歴 2017/04/05 水元：新規作成 <br />
  */
 public class PriceCheck {
-  // 定価
-  private static BigDecimal price;
-  // 会員
-  private static boolean member;
-  // 割引
-  private static String discount;
   // 消費税込
   public static final String TAX = "1.08";
-  
-  /**
-   * 値設定コンストラクタ <br />
-   * 処理に使用するオブジェクトに値を設定する <br />
-   * @param price 定価
-   * @param member 会員
-   * @param discount 割引
-   */
-  PriceCheck( BigDecimal price, boolean member, String discount ) {
-    PriceCheck.price = price;
-    PriceCheck.member = member;
-    PriceCheck.discount = discount;
-  }
-  
+
   /**
    * 金額計算メソッド <br />
    * 定価、会員、割引情報を取得し、最終的な支払金額を計算する <br />
+   * 
+   * @param price 定価
+   * @param member 会員
+   * @param discount 割引
    * @throws Exception 例外
-   * @return total 
+   * @return total 支払金額
    */
-  public static BigDecimal priceCal() throws Exception {
+  static BigDecimal priceCal( BigDecimal price, boolean member, String discount )
+      throws Exception {
     // 支払い金額格納用
     BigDecimal payment = new BigDecimal( 0 );
-    
+
     // 割引の種類で条件分岐
-    if (discount.equals( "0" )) {
+    if ( discount.equals( "0" ) ) {
       payment = price;
-      
-    } else if (discount.equals( "1" )) {
+
+    } else if ( discount.equals( "1" ) ) {
       payment = price.multiply( new BigDecimal( "0.95" ) );
-      
-    } else if (discount.equals( "2" )) {
+
+    } else if ( discount.equals( "2" ) ) {
       payment = price.multiply( new BigDecimal( "0.90" ) );
-      
-    } else if (discount.equals( "3" )) {
+
+    } else if ( discount.equals( "3" ) ) {
       payment = price.multiply( new BigDecimal( "0.80" ) );
-      
+
     } else {
       payment = price;
     }
-    
+
     // 会員の場合、消費税カット
     if ( !member ) {
       payment = payment.multiply( new BigDecimal( TAX ) );
     }
-    
+
     // 最終的な支払金額を返す（小数点以下は繰り上げ）
     return payment.setScale( 0, BigDecimal.ROUND_UP );
   }
